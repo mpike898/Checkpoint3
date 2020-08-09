44c3f438dd8eb61942f50519351075bb432c9121
Matt Pike <mpike_88@hotmail.com>
2020-08-09 14:23:17 -0600

    added API calls and npm dependencies

--- Tests/test.js
+++ Tests/test.js
@@ -2,6 +2,9 @@ var employeePage = {}
 let addEmployee = require('../testAssets/addEmployee')
 let deleteEmployee = require('../testAssets/deleteEmployee')
 let editEmployee = require('../testAssets/editEmployee')
+const axios = require('axios').default;
+var pass = 0
+
 module.exports = {
     beforeEach: browser => {
         employeePage = browser.page.employeePage()
@@ -17,14 +20,11 @@ module.exports = {
         addEmployee(employeePage)
         deleteEmployee(employeePage,'@myEmployee')
     employeePage
-        .pause(5000)
     },
     'Edit Employee and save': browser => {
     
         addEmployee(employeePage)
-        employeePage.pause(5000)
         editEmployee(employeePage)
-        employeePage.pause(5000)
     employeePage
         .verify.visible('@saveBtn')
         .click('@saveBtn')
@@ -49,7 +49,62 @@ module.exports = {
         deleteEmployee(employeePage,'@myEmployee')
 
     },
+// HTTP requests
 
+'Get Employee List': browser => {
+
+    axios.get('https://peaceful-inlet-88854.herokuapp.com/api/employees')
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+
+
+},
+
+'Add Employee via API': browser => {
+
+    axios.post('https://peaceful-inlet-88854.herokuapp.com/api/employees',{
+        id: '699',
+        name: 'Matt Pike',
+        phone: '1251261279',
+        email: 'mattp@gmail.com',
+        title: 'The coolest'
+
+    })
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+
+
+},
+
+'Edit Employee via API': browser => {
+
+    axios.put('https://peaceful-inlet-88854.herokuapp.com/api/employees/699?name=Matt Pikee&phone=1234567899&email=mpike@gmail.com&title=I did it')
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+
+
+},
+
+'Delete Employee via API': browser => {
+
+    axios.delete('https://peaceful-inlet-88854.herokuapp.com/api/employees/699')
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+
+
+},
 
 
 }
